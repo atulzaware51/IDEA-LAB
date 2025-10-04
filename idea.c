e9e843e7c807e0a9a864ded9fcef0bb9f53a77ee
atulzaware51 <atulzaware1411@gmail.com>
2025-10-03 22:43:14 -0700

    Create idea.c

---
+++ idea.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a ,b;
+	printf("\n enter number");
+	scanf("%d",&a);
+	if(a%2==0){
+		printf("the number is prime\n");
+	}
+	else 
+	printf("the number is not prime");
+	
+	return 0;
+}
